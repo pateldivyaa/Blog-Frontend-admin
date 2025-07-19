8c7ebe5905420a5623b78105eea28babba9d8d4b
pateldivyaa <pateldivyad3233@gmail.com>
2025-07-19 22:11:08 +0530

    Add complete backend project

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
 
 // Create axios instance with default config
 const apiClient = axios.create({
